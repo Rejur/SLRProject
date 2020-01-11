9fd6931c6be73292fc89cdb69a7a2eb8a10543fd
Rejur <wwwfeng321@gmail.com>
2020-01-11 13:30:40 +0800

    finished Fast_Compressed_Least_Squares(no debug)

--- Fast_Compressed_Least_Squares.m
+++ Fast_Compressed_Least_Squares.m
@@ -1,9 +1,10 @@
-function [M] = Fast_Compressed_Least_Squares(X, f, cu, cv, alpha, beta)
+function [M, time] = Fast_Compressed_Least_Squares(X, f, cu, cv, alpha, beta, gamma)
     %%Fast Compressed Least-Squares.
     %%Get M.
 
     %% init parameters.
     tic;
+    k = 20;
     Pi = [1, 0, 0 ; 0, 1, 0 ; 0, 0, 1 ];
     K = [f, 0, cu ; 0, f, cv; 0, 0, 1, 0];
     % invC = inv(Pi * K);
@@ -20,11 +21,11 @@ function [M] = Fast_Compressed_Least_Squares(X, f, cu, cv, alpha, beta)
         x_ip1_r = X(7:8,i);
         X_i_l = (Pi * K) \ [x_i_l; 1];
         Ail = [zero3', f * X_i_l', (cv - x_ip1_l(2)) * X_i_l', 0, f, (cv - x_ip1_l(2)), 0;
-            -f * X_i_l', zero3', (x_ip1_l(1) - cu) * X_i_l', -f, 0, (x_ip1_l(1) - cu), alpha;
-            f * x_ip1_l(2) * X_i_l', -f * x_ip1_l(1) * X_i_l', (cu * x_ip1_l(2) - cv * x_ip1_l(1)) * X_i_l', f * x_ip1_l(2), -f * x_ip1_l(1), (cu * x_ip1_l(2) - cv * x_ip1_l(1)), beta];
-        Air = [zero3', f * X_i_l', (cv - x_ip1_r(2)) * X_i_l', 0, f, (cv - x_ip1_r(2)), 0;
-            -f * X_i_l', zero3', (x_ip1_r(1) - cu) * X_i_l', -f, 0, (x_ip1_r(1) - cu), alpha;
-            f * x_ip1_r(2) * X_i_l', -f * x_ip1_r(1) * X_i_l', (cu * x_ip1_r(2) - cv * x_ip1_r(1)) * X_i_l', f * x_ip1_r(2), -f * x_ip1_r(1), (cu * x_ip1_r(2) - cv * x_ip1_r(1)), beta];
+            -f * X_i_l', zero3', (x_ip1_l(1) - cu) * X_i_l', -f, 0, (x_ip1_l(1) - cu), 0;
+            f * x_ip1_l(2) * X_i_l', -f * x_ip1_l(1) * X_i_l', (cu * x_ip1_l(2) - cv * x_ip1_l(1)) * X_i_l', f * x_ip1_l(2), -f * x_ip1_l(1), (cu * x_ip1_l(2) - cv * x_ip1_l(1)), 0];
+        Air = [zero3', f * X_i_l', (cv - x_ip1_r(2)) * X_i_l', 0, f, (cv - x_ip1_r(2)), alpha;
+            -f * X_i_l', zero3', (x_ip1_r(1) - cu) * X_i_l', -f, 0, (x_ip1_r(1) - cu), beta;
+            f * x_ip1_r(2) * X_i_l', -f * x_ip1_r(1) * X_i_l', (cu * x_ip1_r(2) - cv * x_ip1_r(1)) * X_i_l', f * x_ip1_r(2), -f * x_ip1_r(1), (cu * x_ip1_r(2) - cv * x_ip1_r(1)), gamma];
         
         Al = [Al; Ail];
         Ar = [Ar; Air];
@@ -36,15 +37,13 @@ function [M] = Fast_Compressed_Least_Squares(X, f, cu, cv, alpha, beta)
     x = lsqnonlin(fun,x0,[],[],options);
     
     
-
-    
     
     %% Do some final work.
-    M = x;
+    trash, M = EM(x, Gamma);
     time = toc;
 end
 
-function [value] = EM(w, Gamma)
+function [value, EXP] = EM(w, Gamma)
     Z = [0 -w(3) -w(2) w(4); w(3) 0 -w(1) w(5); -w(2) w(1) 0 w(6); 0 0 0 1];
     theta = sqrt(w(1)^2 + w(2)^2 + w(3)^2);
     I = eye(4);
